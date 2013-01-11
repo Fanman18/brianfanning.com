008db54d1352c8591f527938cfeb73034e70d894
Brian Fanning <me@brianfanning.com>
2013-01-10 22:42:15 -0400

    Initial commit of working files

---
+++ footer.php
@@ -0,0 +1,24 @@
+
+        <footer>
+            
+
+        </footer> 
+        
+        <script>
+            head.js(
+                    "js/vendor/modernizr-2.6.2.min.js",
+                    "js/vendor/jquery-1.8.3.min.js",
+                    "js/plugins.js",
+                    "js/main.js"
+                );
+        </script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php include('header.php'); ?>
+        
+        <section id="container">
+            <!-- Add your site or application content here -->
+            <p>Hello world! This is HTML5 Boilerplate.</p>
+        </section>
+
+<?php include('footer.php'); ?>      
+        
